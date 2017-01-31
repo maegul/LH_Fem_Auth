ba99f3ea74ae4602e8b091eaf85ce93e91553b2e
Errol Lloyd <errolkjlloyd@gmail.com>
2017-01-31 22:27:43 +1100

    getting started 2

---
+++ bees_scat/main.js
@@ -0,0 +1,27 @@
+
+
+//
+
+// Vertical Beeswarm for Disciplines with Overall-allCount
+// With interpolated data into the future
+
+// Get adjacent line plot
+
+// select discipline to open journals underneath
+
+// Have journls with same line plot (layout?!)
+
+
+d3.json('data_no_list_no_dup_disc.json', function(data){
+
+
+console.log(data)
+interpolate_years(data)
+
+console.log(data)
+
+
+
+
+}
+)
